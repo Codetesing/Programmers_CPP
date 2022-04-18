b75f3fe68b4dc1e76a37b0433bf5793f2d3ca7ef
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-04-18 10:42:02 +0900

    약수의 개수와 덧셈
    약수의 개수와 덧셈

---
+++ Level 1/EX-17.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+
+    for (int i = left; i <= right; i++)
+    {
+        if (sqrt(i) == round(sqrt(i)))
+            answer -= i;
+        else
+            answer += i;
+    }
+    return answer;
+}
