cfdfd4a3fb86db13ff2f9c620a18d5ca794cd5a1
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-04-28 12:19:35 +0900

    오픈채팅방
    오픈채팅방

--- Level 2/EX-02.cpp
+++ Level 2/EX-02.cpp
@@ -2,13 +2,11 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <map>
+#include <utility>
 
 using namespace std;
-
-typedef struct user {
-    string uid;
-    string name;
-}user;
 
 vector<string> TRANS_RECORD(string s)
 {
@@ -18,30 +16,51 @@ vector<string> TRANS_RECORD(string s)
     for (int i = 0; i < s.size(); i++)
         if (s[i] == ' ')
         {
-            temp.push_back(s.substr(rev, rev - i));
+            temp.push_back(s.substr(rev, i));
             rev = i + 1;
+            break;
         }
+
+    if (temp[0] == "Leave")
+        temp.push_back(s.substr(rev));
+    else
+        for(int i = 0; i < s.size() - rev; i++)
+            if (s[rev + i] == ' ')
+            {
+                temp.push_back(s.substr(rev, i));
+                temp.push_back(s.substr(rev + i + 1, s.size() - rev - i - 1));
+                break;
+            }
 
     return temp;
 }
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
-    vector<user> enter;
-    vector<user> leave;
-    vector<user> change;
     vector<vector<string>> translated_record;
+    map<string, string> user;
 
     for (int i = 0; i < record.size(); i++)
         translated_record.push_back(TRANS_RECORD(record[i]));
 
     for (int i = 0; i < translated_record.size(); i++)
     {
-        for (int j = 0; j < translated_record[i].size(); j++)
-            cout << translated_record[i][j] << ' ';
-        cout << endl;
+        if (translated_record[i][0] != "Leave")
+        {
+            if (user.find(translated_record[i][1]) == user.end())
+                user.insert(pair<string, string>(translated_record[i][1], translated_record[i][2]));
+            else
+                user[translated_record[i][1]] = translated_record[i][2];
+        }
     }
 
+    for (int i = 0; i < translated_record.size(); i++)
+    {
+        if (translated_record[i][0] == "Enter")
+            answer.push_back(user[translated_record[i][1]] + "´ÔÀÌ µé¾î¿Ô½À´Ï´Ù.");
+        else if(translated_record[i][0] == "Leave")
+            answer.push_back(user[translated_record[i][1]] + "´ÔÀÌ ³ª°¬½À´Ï´Ù.");
+    }
     return answer;
 }
 
