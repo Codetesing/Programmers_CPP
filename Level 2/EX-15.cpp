5ff9337633c040bb5a16dc44d17b3a0ae5be2c16
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-05-23 19:49:49 +0900

    조이스틱
    조이스틱

---
+++ Level 2/EX-15.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string name)
+{
+    int answer = 0;
+    int shift = name.length() - 1;
+    for (int i = 0; i < name.length(); i++)
+    {
+        if (name[i] == 'A')
+        {
+            int target = i;
+            while (target < name.length() && name[target] == 'A')
+                target += 1;
+            int left = i == 0 ? 0 : i - 1;
+            int right = name.length() - target;
+            shift = min(shift, left + right + min(left, right));
+        }
+    }
+    answer += shift;
+    for (auto c : name)
+        answer += min(c - 'A', 'Z' - c + 1);
+    return answer;
+}
