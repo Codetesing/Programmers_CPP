f42638156d0a0bd4ff9c5d3af25c63853ba251d6
Codetesting <37256601+Codetesing@users.noreply.github.com>
2023-02-23 11:43:02 +0900

    유사 칸토어 비트열
    https://school.programmers.co.kr/learn/courses/30/lessons/148652

---
+++ Level 2/EX-68.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, long long l, long long r) {
+    int answer = 0;
+
+    for (long long i = l; i <= r; i++) {
+        bool flag = true;
+        
+        if (i % 5 == 3)
+            continue;
+        
+        long long temp = i;
+        long long result;
+        
+        while (1) {
+            if(temp % 5 == 0)
+                temp = temp / 5;
+            else
+                temp = (temp / 5) + 1;
+
+            if(temp % 5 == 3) {
+                flag = false;
+                break;
+            }
+            else if(temp <= 5) {
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag) 
+            answer++;
+    }
+
+    return answer;
+}
